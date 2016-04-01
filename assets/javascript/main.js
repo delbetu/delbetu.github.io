c0bd69384fa8373cfd08f1648532fec8e7702c31
Marcos Bellucci <delbetu@gmail.com>
2016-04-01 10:44:53 -0300

    Move contact_me scroll animation to main js file.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,16 @@
+function scrollTo(number) {
+  $("html, body").animate(
+    { scrollTop: number }, 1000
+  );
+}
+
 $(document).ready(function() {
   $('[data-toggle="tooltip"]').tooltip();
+
+  $(".hire-me").click(function(event) {
+    event.preventDefault();
+
+    var viewPortSize = $(window).height();
+    scrollTo(viewPortSize);
+  });
 });
